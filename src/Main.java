a1fcd8dd3d828d46455b3acb720a41e63e3617f3
Fabiola Magalhaes <fabimagal99@gmail.com>
2023-05-17 13:08:58 -0300

    método para identificar maior e menor idades.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 import Entidades.Pessoa;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        Pessoa pessoa1 = new Pessoa("Patrícia", 21);
-        Pessoa pessoa2 = new Pessoa("Sérgio", 60);
-        Pessoa pessoa3 = new Pessoa("Júlia", 35);
+        ArrayList<Pessoa> pessoas = new ArrayList<>();
 
+        Pessoa pessoa1 = new Pessoa("Patrícia", 21);
+        pessoas.add(pessoa1);
+        Pessoa pessoa2 = new Pessoa("Sérgio", 60);
+        pessoas.add(pessoa2);
+        Pessoa pessoa3 = new Pessoa("Júlia", 8);
+        pessoas.add(pessoa3);
+
+        for (Pessoa pessoa: pessoas) {
+            pessoa.compararIdades();
+        }
 
     }
 }
